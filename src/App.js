b84037a584ebb8e7b5ec3005b2af5d72cd42e695
danju4rizzl <danjumashiwaju@gmail.com>
2021-07-12 17:18:41 +0200

    react-query to to get user's city

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { RecoilRoot } from 'recoil';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import CalendarWidget from './components/CalendarWidget';
 import GreetingWidget from './components/GreetingWidget';
 import WeatherWidget from './components/WeatherWidget';
@@ -11,48 +12,52 @@ import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from './components/UI/Card';
-
+import { ReactQueryDevtools } from 'react-query/devtools';
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const App = () => {
-  // layout is an array of objects, see the demo for more complete usage
+  const queryClient = new QueryClient();
+
   return (
     <RecoilRoot>
-      <main className="main ">
-        <div className="d-flex px-5 py-3 align-items-center justify-content-between">
-          <GreetingWidget />
-          <CalendarWidget />
-        </div>
-        <ResponsiveGridLayout
-          className="layout"
-          margin={[20, 20]}
-          containerPadding={[50, 20]}
-          autoSize={true}
-          verticalCompact={true}
-          rowHeight={25}
-          cols={{ lg: 3, md: 3, sm: 2, xs: 1, xxs: 1 }}
-          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 360 }}
-          isResizable={false}
-          useCSSTransforms={false}
-          isBounded={true}
-        >
-          <div key="0" data-grid={{ x: 0, y: 0, w: 1, h: 8 }}>
-            <Card widgetTitle="Covid-19 Update" widget={<CovidWidget />} />
+      <QueryClientProvider client={queryClient}>
+        <main className="main ">
+          <div className="d-flex px-5 py-3 align-items-center justify-content-between">
+            <GreetingWidget />
+            <CalendarWidget />
           </div>
-          <div key="1" data-grid={{ x: 1, y: 0, w: 1, h: 8 }}>
-            <Card widgetTitle="Weather" widget={<WeatherWidget />} />
-          </div>
-          <div key="2" data-grid={{ x: 2, y: 0, w: 1, h: 8 }}>
-            <Card widgetTitle="Favorites" widget={<FavoritesWidget />} />
-          </div>
-          <div key="3" data-grid={{ x: 0, y: 18, w: 2, h: 8 }}>
-            <Card widgetTitle="Task Maker" widget={<TasksWidget />} />
-          </div>
-          <div key="4" data-grid={{ x: 2, y: 2, w: 1, h: 8 }}>
-            <Card widgetTitle="Daily Quote" widget={<QuotesWidget />} />
-          </div>
-        </ResponsiveGridLayout>
-      </main>
+          <ResponsiveGridLayout
+            className="layout"
+            margin={[20, 20]}
+            containerPadding={[50, 20]}
+            autoSize={true}
+            verticalCompact={true}
+            rowHeight={25}
+            cols={{ lg: 3, md: 3, sm: 2, xs: 1, xxs: 1 }}
+            breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 360 }}
+            isResizable={false}
+            useCSSTransforms={false}
+            isBounded={true}
+          >
+            <div key="0" data-grid={{ x: 0, y: 0, w: 1, h: 8 }}>
+              <Card widgetTitle="Covid-19 Update" widget={<CovidWidget />} />
+            </div>
+            <div key="1" data-grid={{ x: 1, y: 0, w: 1, h: 8 }}>
+              <Card widgetTitle="Weather" widget={<WeatherWidget />} />
+            </div>
+            <div key="2" data-grid={{ x: 2, y: 0, w: 1, h: 8 }}>
+              <Card widgetTitle="Favorites" widget={<FavoritesWidget />} />
+            </div>
+            <div key="3" data-grid={{ x: 0, y: 18, w: 2, h: 8 }}>
+              <Card widgetTitle="Task Maker" widget={<TasksWidget />} />
+            </div>
+            <div key="4" data-grid={{ x: 2, y: 2, w: 1, h: 8 }}>
+              <Card widgetTitle="Daily Quote" widget={<QuotesWidget />} />
+            </div>
+          </ResponsiveGridLayout>
+        </main>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 };
--- src/components/CovidWidget/index.js
+++ src/components/CovidWidget/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchUserCity = async () =>
+  await axios('http://ip-api.com/json').then((res) => res.data.city);
 
 const CovidWidget = () => {
+  // Queries
+  const { data } = useQuery('userCity', fetchUserCity, {
+    refetchOnWindowFocus: false,
+  });
+
   return (
-    <>
-      <div className="covid">Hello from Covid 19</div>
-    </>
+    <div className="">
+      <div className="covid">
+        Hello from Covid 19 in
+        <p>{data}</p>
+      </div>
+    </div>
   );
 };
 
+/*
+const CovidWidget = () => {
+  // Queries
+  const { isLoading, error, data } = useQuery('userIP', () => {
+    return axios('http://ip-api.com/json').then((res) => res.data);
+  });
+  
+  if (isLoading) return 'Loading...';
+  
+  if (error) return 'An error has occurred: ' + error.message;
+  
+  const userCity = !isLoading && data.city;
+  return (
+    <div className="">
+    <div className="covid">
+    Hello from Covid 19 in {userCity}
+    <p></p>
+    </div>
+    </div>
+    );
+  };
+  
+  */
 export default CovidWidget;
