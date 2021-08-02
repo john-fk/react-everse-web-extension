3f1eaa62b2b16379b839532a488f8a06bcae0560
danju4rizzl <danjumashiwaju@gmail.com>
2021-08-02 11:02:56 +0200

    updated weather widget

--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -3,59 +3,53 @@ import WeatherCity from './WeatherCity';
 import WeatherUnit from './WeatherUnit';
 import WeatherCondition from './WeatherCondition';
 import axios from 'axios';
+import { useQuery } from 'react-query';
 import useAxios from 'axios-hooks';
 import BarLoader from 'react-spinners/BarLoader';
 import { css } from '@emotion/core';
 import { MdRefresh } from 'react-icons/md';
-import './index.scss';
+import { useRecoilValue } from 'recoil';
+import { currentUserIp } from '../../EverseStates';
+import './WeatherWidget.scss';
 
 const WeatherWidget = () => {
-  const [city, setCity] = useState([]);
+  // const [city, setCity] = useState([]);
+  const ipData = useRecoilValue(currentUserIp);
+  const { city, country, lat, lon, status } = ipData;
+  const hasIpLoaded = ipData.status;
 
-  // Can be a string as well. Need to ensure each key-value pair ends with ;
-  const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: red;
-  `;
-
-  const getCity = axios('http://ip-api.com/json');
-  getCity
-    .then((res) => setCity(res.data.city))
-    .catch((err) => `Oops there was an error ${err}`);
-
-  const requestOptions = {
-    method: 'GET',
-    url: `https://api.openweathermap.org/data/2.5/weather`,
-    params: {
-      q: city,
-      appid: process.env.OPEN_WEATHER_MAP_API_KEY,
-      units: 'imperial',
+  const { data: weatherData, isLoading, isError } = useQuery(
+    ['userWeatherData'],
+    async () => {
+      const res = await axios(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
+      );
+      const fetchedData = await res.data;
+      // TODO get the data to wait before fetching
+      console.log(fetchedData);
+      return fetchedData;
     },
-  };
+    {
+      enabled: !!hasIpLoaded,
+    }
+  );
 
-  const [{ data, loading, error }, reFetchData] = useAxios(requestOptions);
+  isLoading && <p>Loading...</p>;
 
-  if (loading)
-    return <BarLoader loading={true} css={override} size={10} color="#FFF" />;
+  const celsiusToFahrenheit = (celsius) => (celsius * 9) / 5 + 32;
+  const fahrenheitToCelsius = (fahrenheit) => ((fahrenheit - 32) * 5) / 9;
 
-  if (error) return <p>Error!</p>;
-
+  /*
+  ! Render the data to the dom
   console.log(data);
+   */
+
   return (
-    <div className="bg1">
-      <div className="weather">
-        <div className="weather__temp">
-          <WeatherCity city={city} />
-          <WeatherUnit data={data} />
-          <WeatherCondition conditionData={data.weather} />
-        </div>
-        <MdRefresh
-          title="Refresh"
-          className="weather__refresh"
-          onClick={() => reFetchData()}
-        />
-      </div>
+    <div className="weather">
+      <p>
+        {city}, {country}
+      </p>
+      <h2>{celsiusToFahrenheit(20)}</h2>
     </div>
   );
 };
