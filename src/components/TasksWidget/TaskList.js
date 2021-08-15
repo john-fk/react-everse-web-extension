04b3ca40780aa7735b68f5ca3db637bac1fc8a5a
danju4rizzl <danjumashiwaju@gmail.com>
2021-08-15 22:00:25 +0200

    Updated task widget layout

--- src/components/TasksWidget/TaskList.js
+++ src/components/TasksWidget/TaskList.js
@@ -3,8 +3,8 @@ import Task from './Task';
 
 const TaskList = ({ tasks, onDelete }) => {
   return (
-    <div className="task__list my-2">
-      <ul className="list-group mr-1 flex-column-reverse">
+    <div className="task__list mt-4">
+      <ul className="list-group mr-2 ">
         {tasks.map((item) => (
           <Task task={item} key={item.id} onDelete={onDelete} />
         ))}
